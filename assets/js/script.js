d520eb9e75a60c858a2274d277385a9ce9d56e60
BJ <158008244+behzad17@users.noreply.github.com>
2024-09-06 21:32:23 +0000

    add ffunctions to the game on sj file

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswers(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+    
+}
+function displayMultiplyQuestion(){
+    
+}
